122d36baf6f2b709244750d4da8ba980a958c137
jules viot <jules.viot2@gmail.com>
2019-09-14 23:49:14 +0200

    first launch screen + main(buttons + label)

--- MHR/ViewController.swift
+++ MHR/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
 
 
 }
